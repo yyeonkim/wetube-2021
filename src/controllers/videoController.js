aa23d6a6493be2ccb5041bc88b71c27997eaf3bc
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2021-06-18 16:35:20 +0900

    pug 적용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,6 @@
-export const trending = (req, res) => res.send("Trending Video");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video ID #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit Video");
-};
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => {
