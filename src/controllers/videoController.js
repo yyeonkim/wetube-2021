95e5403b42989f191110ae562b679e8fcb973435
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2021-06-18 17:37:24 +0900

    Iteration으로 배열 요소 전달

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,7 @@
-const user = {
-  username: "Yeon",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [];
+  res.render("home", { pageTitle: "Home", videos: videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", user: user });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Video");
