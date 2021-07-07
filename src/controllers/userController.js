a1fc4434784ee6a09af24a1e1f625e4b9f62b0e7
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2021-07-07 17:35:56 +0900

    회원가입 페이지 생성

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,18 @@
-export const join = (req, res) => res.send("Join in");
+import User from "../models/User";
+
+export const getJoin = (req, res) =>
+  res.render("join", { pageTitle: "회원가입" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
