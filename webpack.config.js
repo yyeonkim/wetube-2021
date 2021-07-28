8ca85eb103413e85dd5e0a008db48d4128e6a288
Yongyeon Kim <yeonlotus@ewhain.net>
2021-07-28 20:08:19 +0900

    nodemon, webpack 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
@@ -12,6 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "assets"),
     filename: "js/main.js",
+    clean: true,
   },
   module: {
     rules: [
