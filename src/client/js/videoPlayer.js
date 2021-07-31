3899ea5ff18be9bb09fa6575ae3a274fff6d8759
Yongyeon Kim <yeonlotus@ewhain.net>
2021-07-31 14:00:07 +0900

    Video play and pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,26 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handlePause = (e) => (playBtn.innerText = "Play");
+const handlePlay = (e) => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {
+  // 비디오 소리가 나면 음소거,
+  // 아니면 소리가 나게 한다
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
