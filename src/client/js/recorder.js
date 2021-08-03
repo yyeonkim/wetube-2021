e36a36a872970ed6566f966f65bd96fedbaad1fb
Yongyeon Kim <yeonlotus@ewhain.net>
2021-08-03 20:16:38 +0900

    영상 녹화 기능 추가

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,41 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
+let stream;
+let recorder;
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+const handleDownload = () => {};
+
+const handleStop = () => {
+  startBtn.innerText = "다운로드";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "녹화중지";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: true,
+    video: false,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
