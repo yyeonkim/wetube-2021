62f809f24e9b247a4a78141049e40f456d6b5b2a
Yongyeon Kim <yeonlotus@ewhain.net>
2021-08-04 10:20:10 +0900

    녹화영상 다운로드

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,8 +2,18 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyVideo";
+  document.body.appendChild(a);
+  a.click();
+  startBtn.innerText = "녹화하기";
+  startBtn.removeEventListener("click", handleDownload);
+  startBtn.addEventListener("click", handleStart);
+};
 
 const handleStop = () => {
   startBtn.innerText = "다운로드";
@@ -16,9 +26,9 @@ const handleStart = () => {
   startBtn.innerText = "녹화중지";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
-    const videoFile = URL.createObjectURL(event.data);
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
