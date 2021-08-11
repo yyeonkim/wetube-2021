75a19a7a0e60733581883acf5384a00d19d80393
Yongyeon Kim <yeonlotus@ewhain.net>
2021-08-11 15:12:19 +0900

    Comment id 가져오기

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,17 +3,25 @@ const { async } = require("regenerator-runtime");
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addComment = (text) => {
+const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
   const profileImg = document.createElement("img");
+  const div = document.createElement("div");
   const span = document.createElement("span");
+  const span2 = document.createElement("span");
   const { avatar } = videoComments.dataset;
+  newComment.dataset.commentId = id;
   profileImg.src = `/${avatar}`;
   span.innerText = `${text}`;
+  span2.innerText = "삭제";
   newComment.className = "video__comment";
+  span.className = "comment__text";
+  span2.className = "comment__delete";
   newComment.appendChild(profileImg);
-  newComment.appendChild(span);
+  newComment.appendChild(div);
+  div.appendChild(span);
+  div.appendChild(span2);
   videoComments.prepend(newComment);
 };
 
@@ -25,16 +33,17 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
-  textarea.value = "";
-  if (status === 201) {
-    addComment(text);
+  if (response.status === 201) {
+    const { newCommentId } = await response.json();
+    textarea.value = "";
+    addComment(text, newCommentId);
   }
 };
 
