deb24b467e7ec8a1c588a2ad64d6d5b851a57651
Yongyeon Kim <yeonlotus@ewhain.net>
2021-08-24 10:56:56 +0900

    함수 이름 수정
    handleClick --> handleDelete

--- src/client/js/alertMessage.js
+++ src/client/js/alertMessage.js
@@ -2,7 +2,7 @@ const { async } = require("regenerator-runtime");
 
 const deleteBtn = document.getElementById("deleteBtn");
 
-const handleClick = async () => {
+const handleDelete = async () => {
   const deleteRes = confirm(
     "탈퇴할 경우 영상과 댓글이 모두 삭제됩니다.\n탈퇴하시겠습니까?"
   );
@@ -10,9 +10,9 @@ const handleClick = async () => {
     const response = await fetch("/api/users/delete", { method: "delete" });
     if (response.status === 201) {
       alert("탈퇴가 완료되었습니다.");
-      location.href = "https://wetubecc.herokuapp.com/"; // homepage
+      location.href = "/";
     }
   }
 };
 
-deleteBtn.addEventListener("click", handleClick);
+deleteBtn.addEventListener("click", handleDelete);
